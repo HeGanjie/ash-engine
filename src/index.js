dba5382350ed9371a93cd8392914a987b061c9b7
Bruce He <heganjie@gmail.com>
2018-02-26 16:14:43 +0800

    add frame rate inspector

--- src/index.js
+++ src/index.js
@@ -23,12 +23,25 @@ let camera = new Camera(400, 300);
 camera.position = new Vector3(0, 0, 10);
 camera.target = new Vector3(0, 0, 0);
 
+// https://stackoverflow.com/a/5111475/1745885
+let filterStrength = 2;
+let frameTime = 0, lastLoop = Date.now(), thisLoop;
+
 function drawingLoop() {
   camera.render(scene, ctx)
   mesh.rotation.x += 0.01;
   mesh.rotation.y += 0.01;
   requestAnimationFrame(drawingLoop);
+
+  let thisFrameTime = (thisLoop = Date.now()) - lastLoop;
+  frameTime += (thisFrameTime - frameTime) / filterStrength;
+  lastLoop = thisLoop;
 }
 
 
 requestAnimationFrame(drawingLoop)
+
+let fpsOut = document.getElementById('fps');
+setInterval(function() {
+  fpsOut.innerText = (1000/frameTime).toFixed(1) + " fps";
+},1000);
