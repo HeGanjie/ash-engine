7cc69fff5ec3ec629024c571d3ad8851ead7c0c5
heganjie <heganj@qq.com>
2019-10-27 21:07:33 +0800

    add box diffuse and specular texture

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { Camera } from "./webgl-camera";
 import Stats from "stats.js";
 import {DistantLight} from "./distant-light";
 import {PointLight} from "./point-light";
-import wallImg from './assets/wall.jpg'
+import wallImgUrl from './assets/wall.jpg'
+import boxImgUrl from './assets/container2.png'
+import boxSpecularUrl from './assets/container2_specular.png'
 
 var stats = new Stats();
 stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
@@ -17,16 +19,28 @@ if (!ctx) {
   throw new Error("Not support webgl2");
 }
 
-async function genScene() {
-  let wallImgInst = new Image();
-  wallImgInst.src = wallImg;
+async function loadImage(assetsUrl) {
+  let image = new Image();
+  image.src = assetsUrl;
   await new Promise(resolve => {
-    wallImgInst.addEventListener('load', resolve);
+    let callback = () => {
+      image.removeEventListener('load', callback)
+      resolve()
+    }
+    image.addEventListener('load', callback);
   })
+  return image
+}
+
+async function genScene() {
+  let wallImg = await loadImage(wallImgUrl)
+  let boxImg = await loadImage(boxImgUrl)
+  let boxSpecularImg = await loadImage(boxSpecularUrl)
 
   let cubeMaterial = new Material({
     color: { r: 1, g: 1, b: 1 },
-    // diffuseMap: wallImgInst,
+    diffuseMap: boxImg,
+    specularMap: boxSpecularImg,
     kS: 0.15,
     kD: 1 - 0.15,
     specularExp: 250
@@ -35,7 +49,7 @@ async function genScene() {
 
   let planeMaterial = new Material({
     // color: { r: 0.5, g: 0.5, b: 0.5 },
-    diffuseMap: wallImgInst,
+    diffuseMap: wallImg,
     kS: 0.04,
     kD: 1 - 0.04,
     specularExp: 2
@@ -95,5 +109,4 @@ genScene().then(scene => {
   requestAnimationFrame(drawingLoop);
   // setInterval(drawingLoop, 5000);
 })
-
 
